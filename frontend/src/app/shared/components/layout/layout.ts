1900be3cbcfb4d7eda4ffe7199602ece3686ee9f
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2026-04-29 16:55:43 -0400

    fix: corrigido links
    - RouterLink, RouterLinkActive

--- frontend/src/app/shared/components/layout/layout.ts
+++ frontend/src/app/shared/components/layout/layout.ts
@@ -1,10 +1,10 @@
 import { AuthService } from "@/core/services/auth.service";
 import { Component, computed, inject } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
